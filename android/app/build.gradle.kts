1cdbcde2b5e68a11ffe078900b31cdf421400537
monroju <monroigjg@gmail.com>
2026-05-12 14:49:34 -0400

    Bump Android to v1.4.0 (versionCode 18) for Fast-Track Eligibility v2 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.gothere.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.3.1"
+        versionCode = 18
+        versionName = "1.4.0"
         vectorDrawables.useSupportLibrary = true
     }
 
